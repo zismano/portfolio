33293f7b83108ad190e21f591af19ea83a6e50d3
Ofir Zisman <zismanofir@gmail.com>
2018-05-07 17:36:25 -0700

    Add event listener when hovers on skills

---
+++ index.js
@@ -0,0 +1,10 @@
+let skills = document.querySelectorAll(".skill");
+
+for (let i = 0; i < skills.length; i++) {
+	skills[i].addEventListener("mouseenter", () => {
+		skills[i].classList.add("hover");
+	})
+	skills[i].addEventListener("mouseleave", () => {
+		skills[i].classList.remove("hover");
+	})
+}

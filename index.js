0a93ee657aceec985e3cae316263769f48b925c3
Ofir Zisman <zismanofir@gmail.com>
2018-05-09 16:28:58 -0700

    Add id to selected section in navbar

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ for (let i = 0; i < skills.length; i++) {
 		skills[i].classList.remove("hover");
 	});
 }
+
+let sections = document.querySelector("#sections");
+
+sections.addEventListener("click", (e) => {
+	console.log(e.target)
+	let lis = sections.children;
+	for (let i = 0; i < lis.length; i++) {
+		if (lis[i].children[0] === e.target) {	// e.target is anchor
+			lis[i].setAttribute("id", "selected");
+		} else {
+	    	lis[i].removeAttribute("id");
+		}
+	}
+})
